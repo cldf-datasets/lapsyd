1e0ce75575d141a4eaf98299bdaa265960e8eccd
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-06-11 18:36:25 +0200

    Start to move code to cldfbench

--- cldfbench_lapsyd.py
+++ cldfbench_lapsyd.py
@@ -1,117 +1,75 @@
-# **NOTE**: While this script is named `cldfbench_eurasianinventories.py`,
-#           it is not integrated with `cldfbench` yet; the name is only
-#           here as a placeholder for future integration.
+import pathlib
 
-from collections import OrderedDict, defaultdict
-from pathlib import Path
-import base64
-import csv
-import hashlib
-import json
-import logging
-
-# Import MPI-SHH libraries
+from cldfbench import CLDFSpec
+from cldfbench import Dataset as BaseDataset
+from clldutils.misc import slug
 from clldutils.path import git_describe
-from pycldf import StructureDataset
-from pyglottolog import Glottolog
+
+# TODO: temporary instantiation of catalogs
+CLTS_PATH = "/home/tresoldi/.config/cldf/clts"
+GLOTTOLOG_PATH = "/home/tresoldi/.config/cldf/glottolog"
 import pyclts
+import pyglottolog
 
-# BIPA features for descriptors
-BIPA_FEATURES = [
-    "advanced",
-    "affricate",
-    "alveolar",
-    "alveolo_palatal",
-    "apical",
-    "approximant",
-    "aspirated",
-    "back",
-    "bilabial",
-    "breathy",
-    "central",
-    "centralized",
-    "close",
-    "close_mid",
-    "cluster",
-    "consonant",
-    "creaky",
-    "dental",
-    "devoiced",
-    "diphthong",
-    "ejective",
-    "epiglottal",
-    "fricative",
-    "front",
-    "glottal",
-    "glottalized",
-    "implosive",
-    "labialized",
-    "labio_dental",
-    "labio_palatal",
-    "labio_velar",
-    "laminal",
-    "lateral",
-    "less_rounded",
-    "long",
-    "lowered",
-    "mid",
-    "mid_centralized",
-    "mid_long",
-    "more_rounded",
-    "nasal",
-    "nasalized",
-    "near_back",
-    "near_close",
-    "near_front",
-    "near_open",
-    "non_syllabic",
-    "open",
-    "open_mid",
-    "palatal",
-    "palatal_velar",
-    "palatalized",
-    "pharyngeal",
-    "pharyngealized",
-    "post_alveolar",
-    "pre_aspirated",
-    "pre_glottalized",
-    "pre_labialized",
-    "pre_nasalized",
-    "raised",
-    "retracted",
-    "retroflex",
-    "rhotacized",
-    "rounded",
-    "sibilant",
-    "stop",
-    "syllabic",
-    "tap",
-    "trill",
-    "ultra_long",
-    "ultra_short",
-    "unrounded",
-    "uvular",
-    "velar",
-    "velarized",
-    "voiced",
-    "voiceless",
-    "vowel",
-    "with_frication",
-]
+CLTS = pyclts.CLTS(CLTS_PATH)
+GLOTTOLOG = pyglottolog.Glottolog(GLOTTOLOG_PATH)
+
+# TODO: replace this function with unicode points + unidecode repr
+# Compute the hash/id for a segment
+def compute_id(segment):
+    import base64, hashlib
+
+    return str(base64.b16encode(hashlib.md5(segment.encode("utf-8")).digest()), "utf-8")
 
 
-def prepare_cldf(glottolog):
-    cldf_dir = Path("cldf")
+# TODO: incorporate in pyclts?
+def build_segment_data(segment_set, feature_list):
+    # Build segment data
+    segments = []
+    for segment in segment_set:
+        # Initialize all features to negative, and then set as positive
+        row = {feature: "-" for feature in feature_list}
+        if not isinstance(CLTS.bipa[segment], pyclts.models.UnknownSound):
+            for feature in CLTS.bipa[segment].featureset:
+                row[feature] = "+"
+
+        # Add other info
+        if row.get("vowel", None) == "+":
+            row["SegmentClass"] = "Vowel"
+        elif row.get("consonant", None) == "+":
+            row["SegmentClass"] = "Consonant"
+        elif row.get("diphthong", None) == "+":
+            row["SegmentClass"] = "Diphthong"
+        else:
+            row["SegmentClass"] = "UnknownSound"
+
+        if not isinstance(CLTS.bipa[segment], pyclts.models.UnknownSound):
+            row["Description"] = CLTS.bipa[segment].name
+        else:
+            row["Description"] = None
+        row["Name"] = segment
+        row["ID"] = compute_id(segment)
+
+        # Update data
+        segments.append(row)
+
+    return segments
+
+
+# TODO: remove this function, integrating to `cldf_specs()`
+def prepare_cldf(features, glottolog_version):
+    # imports for this function
+    from collections import OrderedDict
+    from pycldf import StructureDataset
+
+    cldf_dir = pathlib.Path("cldf")
 
     ds = StructureDataset.in_dir(cldf_dir)
     ds.tablegroup.notes.append(
         OrderedDict(
             [
                 ("dc:title", "environment"),
-                (
-                    "properties",
-                    OrderedDict([("glottolog_version", git_describe(glottolog.repos))]),
-                ),
+                ("properties", OrderedDict([("glottolog_version", glottolog_version)])),
             ]
         )
     )
@@ -122,7 +80,7 @@ def prepare_cldf(glottolog):
         "Contribution_ID",
     )
 
-    ds.add_component("ParameterTable", "SegmentClass", *BIPA_FEATURES)
+    ds.add_component("ParameterTable", "SegmentClass", *features)
     ds.add_component("LanguageTable", "Family_Glottocode", "Family_Name")
     ds.add_table(
         "inventories.csv",
@@ -142,145 +100,112 @@ def prepare_cldf(glottolog):
     return ds
 
 
-# Compute the hash/id for a segment
-def compute_id(segment):
-    return str(base64.b16encode(hashlib.md5(segment.encode("utf-8")).digest()), "utf-8")
+class Dataset(BaseDataset):
+    dir = pathlib.Path(__file__).parent
+    id = "lapsyd"
 
+    def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
+        return CLDFSpec(dir=self.cldf_dir, module="StructureDataset")
 
-def build_segment_data(segment_set, clts):
-    # Build segment data
-    segments = []
-    for segment in segment_set:
-        # Initialize all features to negative, and then set as positive
-        row = {feature: "-" for feature in BIPA_FEATURES}
-        if not isinstance(clts.bipa[segment], pyclts.models.UnknownSound):
-            for feature in clts.bipa[segment].featureset:
-                row[feature.replace("-", "_")] = "+"
+    def cmd_download(self, args):
+        """
+        Download files to the raw/ directory. You can use helpers methods of `self.raw_dir`, e.g.
 
-        # Add other info
-        if row["vowel"]:
-            row["SegmentClass"] = "Vowel"
-        elif row["consonant"]:
-            row["SegmentClass"] = "Consonant"
-        elif row["diphthong"]:
-            row["SegmentClass"] = "Diphthong"
-        else:
-            row["SegmentClass"] = "UnknownSound"
+        >>> self.raw_dir.download(url, fname)
+        """
+        pass
 
-        if not isinstance(clts.bipa[segment], pyclts.models.UnknownSound):
-            row["Description"] = clts.bipa[segment].name
-        else:
-            row["Description"] = None
-        row["Name"] = segment
-        row["ID"] = compute_id(segment)
+    def cmd_makecldf(self, args):
+        """
+        Convert the raw data to a CLDF dataset.
 
-        # Update data
-        segments.append(row)
+        >>> args.writer.objects['LanguageTable'].append(...)
+        """
 
-    return segments
+        # load language mapping and build inventory info
+        languages = []
+        inventories = []
+        glottocodes = {}
+        for row in self.etc_dir.read_csv("languages.csv", dicts=True):
+            # extend language info
+            languages.append(row)
 
+            # Add mapping, if any
+            if row["glottocode"]:
+                glottocodes[row["name"]] = row["glottocode"]
 
-def main(clts_path, glottolog_path):
-    # read raw data
-    raw_data_csv = Path("raw") / "lapsyd.csv"
-    with open(raw_data_csv.as_posix()) as csvfile:
-        raw_data = [row for row in csv.DictReader(csvfile)]
-
-    # Load language mapping
-    languages_csv = Path("etc") / "languages.csv"
-    with open(languages_csv.as_posix()) as csvfile:
-        languages = [row for row in csv.DictReader(csvfile)]
-        glottocodes = {
-            language["name"]: language["glottocode"]
-            for language in languages
-            if language["glottocode"]
-        }
-
-    inventories = []
-    for idx, language in enumerate(languages):
-        # Collect inventory info
-        inventories.append(
-            {
-                "Contributor_ID": None,
-                "ID": idx + 1,
-                "Name": language,
-                "Source": [],
-                "URL": None,
-                "Tones": None,
-            }
-        )
-
-    # Instantiate CLTS and Glottolog
-    clts = pyclts.CLTS(clts_path)
-    glottolog = Glottolog(glottolog_path)
-
-    # Map glottolog from etc/languages.csv
-    languoids = {
-        lang.id: {
-            "Family_Glottocode": lang.lineage[0][1] if lang.lineage else None,
-            "Family_Name": lang.lineage[0][0] if lang.lineage else None,
-            "Glottocode": lang.id,
-            "ID": lang.id,
-            "ISO639P3code": lang.iso_code,
-            "Latitude": lang.latitude,
-            "Longitude": lang.longitude,
-            "Macroarea": lang.macroareas[0].name if lang.macroareas else None,
-            "Name": lang.name,
-        }
-        for lang in glottolog.languoids()
-        if lang.id in glottocodes.values()
-    }
-
-    # Prepare dataset
-    ds = prepare_cldf(glottolog)
-
-    # Iterate over all entries
-    segment_set = set()
-    values = []
-    counter = 1
-    for row in raw_data:
-        # clear segment data
-        segment = row['segments'].strip()
-        if not segment:
-            continue
-
-        if segment[0] == "'":
-            segment = segment[1:-1]
-            marginal = True
-        else:
-            marginal = False
-
-        segment_set.add(segment)
-
-        values.append(
+            # collect inventory info
+            inventories.append(
                 {
-                    "ID": str(counter),
-                    "Language_ID": glottocodes[row['name']],
+                    "Contributor_ID": None,
+                    "ID": slug(row["name"]),
+                    "Name": row["name"],
+                    "Source": [],
+                    "URL": None,
+                    "Tones": None,
+                }
+            )
+
+        # Map glottolog from etc/languages.csv
+        languoids = {
+            lang.id: {
+                "Family_Glottocode": lang.lineage[0][1] if lang.lineage else None,
+                "Family_Name": lang.lineage[0][0] if lang.lineage else None,
+                "Glottocode": lang.id,
+                "ID": lang.id,
+                "ISO639P3code": lang.iso_code,
+                "Latitude": lang.latitude,
+                "Longitude": lang.longitude,
+                "Macroarea": lang.macroareas[0].name if lang.macroareas else None,
+                "Name": lang.name,
+            }
+            for lang in GLOTTOLOG.languoids()
+            if lang.id in glottocodes.values()
+        }
+
+        # Prepare dataset
+        BIPA_FEATURES = list(CLTS.bipa._feature_values.keys())
+        BIPA_FEATURES = [f.replace("-", "_") for f in BIPA_FEATURES]
+        my_ds = prepare_cldf(BIPA_FEATURES, git_describe(GLOTTOLOG.repos))
+
+        # Iterate over raw data
+        segment_set = set()
+        values = []
+        counter = 1
+        for idx, row in enumerate(self.raw_dir.read_csv("lapsyd.csv", dicts=True)):
+            # clear segment data
+            segment = row["segments"].strip()
+            if not segment:
+                continue
+
+            if segment[0] == "'":
+                segment = segment[1:-1]
+                marginal = True
+            else:
+                marginal = False
+
+            segment_set.add(segment)
+
+            values.append(
+                {
+                    "ID": str(idx + 1),
+                    "Language_ID": glottocodes[row["name"]],
                     "Marginal": marginal,
                     "Parameter_ID": compute_id(segment),  # Compute
                     "Value": segment,
                     "Source": [],  # TODO
                 }
             )
-        counter += 1
 
-    # Build segment data
-    segments = build_segment_data(segment_set, clts)
+        # Build segment data
+        segments = build_segment_data(segment_set, BIPA_FEATURES)
 
-    # Write data and validate
-    ds.write(
-        **{
-            "ValueTable": values,
-            "LanguageTable": languoids.values(),
-            "ParameterTable": segments,
-            "inventories.csv": inventories,
-        }
-    )
-    ds.validate(logging.getLogger(__name__))
-
-
-if __name__ == "__main__":
-    main(
-        Path("~/.config/cldf/clts").expanduser(),
-        Path("~/.config/cldf/glottolog").expanduser(),
-    )
+        # Write data and validate
+        my_ds.write(
+            **{
+                "ValueTable": values,
+                "LanguageTable": languoids.values(),
+                "ParameterTable": segments,
+                "inventories.csv": inventories,
+            }
+        )
