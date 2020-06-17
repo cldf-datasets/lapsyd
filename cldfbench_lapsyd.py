d91b3b65a9559b18861866009c711376edd25f22
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-06-17 15:55:59 +0200

    Using Lapsyd features

--- cldfbench_lapsyd.py
+++ cldfbench_lapsyd.py
@@ -34,6 +34,7 @@ class Dataset(BaseDataset):
 
         >>> self.raw_dir.download(url, fname)
         """
+
         pass
 
     def cmd_makecldf(self, args):
@@ -43,6 +44,19 @@ class Dataset(BaseDataset):
         >>> args.writer.objects['LanguageTable'].append(...)
         """
 
+        # Instantiate glottolog
+        g = Glottolog(args.glottolog.dir)
+
+        # Load Lapsyd feature mapping and features
+        lapsyd_graphemes = {}
+        lapsyd_features = set()
+        for row in self.etc_dir.read_csv("features.tsv", delimiter="\t", dicts=True):
+            grapheme = unicodedata.normalize("NFC", row["Grapheme"])
+
+            grapheme_features = row["Name"].split()
+            lapsyd_graphemes[grapheme] = grapheme_features
+            lapsyd_features.update(grapheme_features)
+
         # Add components
         args.writer.cldf.add_columns(
             "ValueTable",
@@ -51,7 +65,9 @@ class Dataset(BaseDataset):
             "Contribution_ID",
         )
 
-        args.writer.cldf.add_component("ParameterTable")
+        args.writer.cldf.add_component(
+            "ParameterTable", *[slug(feature) for feature in lapsyd_features]
+        )
         args.writer.cldf.add_component(
             "LanguageTable", "Family_Glottocode", "Family_Name"
         )
@@ -69,9 +85,6 @@ class Dataset(BaseDataset):
             "Tones",
             primaryKey="ID",
         )
-
-        # Instantiate glottolog
-        g = Glottolog(args.glottolog.dir)
 
         # load language mapping and build inventory info
         languages = []
@@ -124,19 +137,13 @@ class Dataset(BaseDataset):
             if not segment:
                 continue
 
-            if segment[0] == "'":
-                segment = segment[1:-1]
-                marginal = True
-            else:
-                marginal = False
-
             segment_set.add(segment)
 
             values.append(
                 {
                     "ID": str(idx + 1),
                     "Language_ID": glottocodes[row["name"]],
-                    "Marginal": marginal,
+                    "Marginal": False,
                     "Parameter_ID": compute_id(segment),  # Compute
                     "Value": segment,
                     "Source": [],  # TODO
@@ -144,10 +151,16 @@ class Dataset(BaseDataset):
             )
 
         # Build segment data
-        segments = [
-            {"Name": unicodedata.normalize("NFC", segment), "ID": compute_id(segment)}
-            for segment in segment_set
-        ]
+        segments = []
+        for segment in segment_set:
+            grapheme = unicodedata.normalize("NFC", segment)
+            entry = {"Name": grapheme, "ID": compute_id(grapheme)}
+            for feature in lapsyd_features:
+                if feature in lapsyd_graphemes[grapheme]:
+                    entry[slug(feature)] = "+"
+                else:
+                    entry[slug(feature)] = "-"
+            segments.append(entry)
 
         # Write data and validate
         args.writer.write(
